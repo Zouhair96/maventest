c63934631ec8b3ff412b22bf019d35f48f970e4d
Zouhair96 <zouhair.benali96@gmail.com>
2020-02-09 14:01:14 +0000

    Adding Application java source

---
+++ src/main/java/org/exemple/demo/App.java
@@ -0,0 +1,14 @@
+package org.exemple.demo;
+
+public class App {
+    
+    public App() {
+        System.out.println ("Hello Zouhair");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Hello Zouhair Again");
+	App app = new App();
+    }
+}
